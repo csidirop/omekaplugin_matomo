765b15f5300f45824ea3b05e2c03a4ee67c1bea5
Christos Sidiropoulos <dev@csidirop.de>
2024-12-03 16:00:59 +0000

    Update config-form with title and description and more classes
    Signed-off-by: Christos Sidiropoulos <dev@csidirop.de>

--- views/admin/config-form.php
+++ views/admin/config-form.php
@@ -5,22 +5,28 @@
 ?>
 
 <div class="field">
-    <div class="two columns alpha">
-        <?php echo get_view()->formLabel('matomo-url', __('Matomo URL')); ?>
+    <div><h2>Main Options</h2></div>
+    <div><p>These options are mandatory and must be set.</p></div>
+    <div class="matomo-url">
+        <div class="two columns alpha">
+            <?php echo $view->formLabel('matomo-url', __('Matomo URL')); ?>
+        </div>
+        <div class="inputs five columns omega">
+            <p class="explanation">
+                <?php echo __('This is the Matomo server URL.'); ?>
+            </p>
+            <?php echo $view->formText('matomo-url', $matomoURL); ?>
+        </div>
     </div>
-    <div class="inputs five columns omega">
-        <p class="explanation">
-            <?php echo __('This is the Matomo server URL.'); ?>
-        </p>
-        <?php echo get_view()->formText('matomo-url', $matomoURL); ?>
-    </div>
-    <div class="two columns alpha">
-        <?php echo get_view()->formLabel('matomo-site-id', __('Matomo Site Id')); ?>
-    </div>
-    <div class="inputs five columns omega">
-        <p class="explanation">
-            <?php echo __('This is the site ID for this omeka instance.'); ?>
-        </p>
-        <?php echo get_view()->formText('matomo-site-id', $matomoSiteId); ?>
+    <div class="matomo-site-id">
+        <div class="two columns alpha">
+            <?php echo $view->formLabel('matomo-site-id', __('Matomo Site Id')); ?>
+        </div>
+        <div class="inputs five columns omega">
+            <p class="explanation">
+                <?php echo __('This is the site ID for this omeka instance.'); ?>
+            </p>
+            <?php echo $view->formText('matomo-site-id', $matomoSiteId); ?>
+        </div>
     </div>
 </div>
